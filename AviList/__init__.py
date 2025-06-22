c8f386a563a226d61ac83aff3dbdf799328b97de
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2025-06-22 14:51:33 -0600

    Create __init__.py

---
+++ AviList/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 22 11:18:06 2025
+
+@author: Thomas Lee
+Rice University
+Department of Earth, Environmental, and Planetary Sciences
+Email: tl165@rice.edu
+
+Citations:
+AviList Core Team. 2025. AviList: The Global Avian Checklist, v2025. https://doi.org/10.2173/avilist.v2025
+"""
+
+all = ['data','taxonomy']
+__version__ = "1.0.0"
